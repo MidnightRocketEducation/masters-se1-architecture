51ac4857b6a4d2757f154e2c52645555e7c97012
MattisJensen <matjen@mail.de>
2025-11-12 15:34:09 +0100

    Initial Spring Setup

---
+++ milk-services/prod-line-sys/prod-line-sys-svc/src/main/java/prod_line_sys_svc/ProdLineSysSvcApplication.java
@@ -0,0 +1,13 @@
+package prod_line_sys_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdLineSysSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdLineSysSvcApplication.class, args);
+	}
+
+}
---
+++ milk-services/prod-line-sys/prod-line-sys-svc/src/test/java/prod_line_sys_svc/ProdLineSysSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package prod_line_sys_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdLineSysSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
